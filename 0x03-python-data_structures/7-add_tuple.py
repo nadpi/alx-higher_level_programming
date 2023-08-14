cf7d7b70db16b9315ea4278ae5fad180119317e2
nada <nadawaled87@gmail.com>
2023-08-14 13:33:30 -0700

    task 7

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    newt = ()
+    for i in range(2):
+        if tuple_a[i]:
+            if i < len(tuple_b):
+                newt = newt+((tuple_a[i]+tuple_b[i]), )
+            else:
+                newt = newt + ((tuple_a[i]), )
+        else:
+            if i < len(tuple_b):
+                newt = newt + ((tuple_b[i]), )
+            else:
+                newt = newt + ((0), )
+    return newt
