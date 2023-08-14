278b1a01ee99101f058e679efeed212ef7ab5a3c
nada <nadawaled87@gmail.com>
2023-08-14 14:25:39 -0700

    task 11

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    if my_list:
+        for i in range(len(my_list)):
+            if i == idx:
+                my_list.remove(my_list[i])
+            else:
+                continue
+        return my_list
