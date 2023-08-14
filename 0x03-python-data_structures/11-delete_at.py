917fc3220cf17fa4caa63c74c548007606cb3025
nada <nadawaled87@gmail.com>
2023-08-14 15:00:53 -0700

    task 11

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,7 +2,9 @@
 def delete_at(my_list=[], idx=0):
     if my_list:
         if idx >= 0 and idx < len(my_list):
-            my_list.remove(my_list[idx])
+            for i in range(len(my_list)):
+                if i == idx:
+                    my_list.remove(my_list[i])
             return my_list
         else:
             return my_list
