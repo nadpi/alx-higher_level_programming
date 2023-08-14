ce8ade62476ac19f7c17207307fcebc9289d9f7d
nada <nadawaled87@gmail.com>
2023-08-14 13:55:46 -0700

    task 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/bin/usr/python3
+def max_integer(my_list=[]):
+    maxnum = my_list[0]
+    if my_list:
+        for i in range(len(my_list)):
+            if my_list[i] > maxnum:
+                maxnum = my_list[i]
+            else:
+                continue
+        return maxnum
+    else:
+        return None
