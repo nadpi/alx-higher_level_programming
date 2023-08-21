897b5bb7120e56bdf59dd6299764482baf0a6648
nada <nadawaled87@gmail.com>
2023-08-21 14:47:27 -0700

    task 1

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    if my_list:
+        newlist = my_list[:]
+        for i in range(len(newlist)):
+            if newlist[i] == search:
+                newlist[i] = replace
+            else:
+                continue
+        return newlist
