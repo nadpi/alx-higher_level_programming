b2d942f1eb9c6083ea08a6c8a7d0af303e213204
nada <nadawaled87@gmail.com>
2023-08-21 14:33:12 -0700

    task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,16 +1,10 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     if matrix:
-        newm = matrix[:]
-        for i in range(len(matrix)):
-            newm.append('[')
-            for j in range(len(matrix[i])):
-                newm.append(matrix[i][j]**2)
-                if j < len(matrix[i]):
-                    newm.append[","]
-                    newm.append[" "]
-            if i < len(matrix):
-                newm.append(']')
-        return newm
+        newmatrix = []
+        for j in range(len(matrix)):
+            newmatrixrow = list(map((lambda x: x ** 2), matrix[j]))
+            newmatrix.append(newmatrixrow)
+        return newmatrix
     else:
         return matrix
