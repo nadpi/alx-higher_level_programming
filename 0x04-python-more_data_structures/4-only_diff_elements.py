b11871f26339fdde37d3af001a771568bff84a6d
nada <nadawaled87@gmail.com>
2023-08-21 15:53:50 -0700

    task 4

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -11,6 +11,9 @@ def only_diff_elements(set_1, set_2):
                     newset.add(i)
             return newset
         else:
-            return set_2
+            return set_1
     else:
-        return set_1
+        if set_2:
+            return set_2
+        else:
+            return set_1
