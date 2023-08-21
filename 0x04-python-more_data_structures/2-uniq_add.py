269ac72294d35d97c3d14bb3c39bdb4330ec1d9f
nada <nadawaled87@gmail.com>
2023-08-21 15:14:19 -0700

    task 2

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    sums = 0
+    flag = True
+    if my_list:
+        for i in range(len(my_list)):
+            for j in range(i + 1, len(my_list)):
+                if my_list[i] == my_list[j]:
+                    flag = False
+                    break
+                else:
+                    flag = True
+            if flag:
+                sums += my_list[i]
+        return sums
+    else:
+        return sums
