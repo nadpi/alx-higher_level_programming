7445c94f52c7d3790e01bf5e942fa45ee9d88404
nada <nadawaled87@gmail.com>
2023-08-22 12:31:18 -0700

    task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if type(roman_string) != str or roman_string == False:
+    if type(roman_string) == str and roman_string:
+        roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        sums = 0
+        if roman_string[0] == "I":
+            sums = -2
+        for c in range(0, len(roman_string)):
+            if roman_string[c] in roman:
+                sums += roman[roman_string[c]]
+        return sums
+    else:
         return 0
-    roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-    sums = 0
-    for c in range(len(roman_string)):
-        if roman_string[c] in roman:
-            sums += roman[roman_string[c]]
-        else:
-            continue
-    if roman_string[0] == "I":
-        sums = sums - 2
-    return sums
 
