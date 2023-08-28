9633cc6e6b4d801155397827b3d92e5ac9563004
nada <nadawaled87@gmail.com>
2023-08-28 08:58:28 -0700

    task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     if my_list:
-        num = ""
         for i in range(x):
             try:
-                num = num + str(my_list[i])
                 print("{}".format(my_list[i]), end="")
             except IndexError:
                 print("")
                 return i
         print("")
-        return i + 1
-    else:
-        return 0
+        return x
