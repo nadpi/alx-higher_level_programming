489b74278f5f0add1fe0b4bff7cb2a90e1ebe421
nada <nadawaled87@gmail.com>
2023-08-29 13:27:27 -0700

    task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,8 +7,6 @@ class Square:
     Attributes:
         size (int): size of square
     """
-
-
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
