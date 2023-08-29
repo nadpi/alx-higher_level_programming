1e6017b0a8f382530f24f5133ca807802947a662
nada <nadawaled87@gmail.com>
2023-08-29 12:50:54 -0700

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" a class Square that defines a square """
+
+
+class Square:
+    """ a square class
+    Attributes:
+        size (int): size of square
+    """
+
+    def __init__(self, size):
+        self.__size = size
