b932224ed52f8078a8a6a459134e9bec17f7109a
nada <nadawaled87@gmail.com>
2023-08-29 13:34:21 -0700

    task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" class Square that defines a square """
+
+
+class Square:
+    """ a class sqaure that has size
+    Attributes:
+        size (int): size of square
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """returns the current square area """
+        return self.__size * self.__size
