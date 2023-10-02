aa845aa43cb445d5371ea8616931f091e91d4cf6
nada <nadawaled87@gmail.com>
2023-10-02 10:26:24 -0700

    UPDATE

--- 0x0A-python-inheritance/0-lookup.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-""" a function that returns the list of
-available attributes and methods of an object """
-
-
-def lookup(obj):
-    """ Prototype: def lookup(obj):
-    Returns a list object """
-    return list(dir(obj))
--- 0x0A-python-inheritance/5-base_geometry.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-""" A class """
-
-
-class BaseGeometry:
-    """ empty class """
-    pass
