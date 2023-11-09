de86d597eb900b06976358d07660c41a490c9e46
nadpi <nadawaled87@gmail.com>
2023-11-08 17:48:14 -0800

    task 0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def lookup(obj):
-    lis = list(dir(obj))
-    return lis
+    """ function that returns the list of available attributes
+    and methods of an object"""
+    return list(dir(obj))
