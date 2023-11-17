6fcaeef84ce234bf6cb98c4e4f904668883fb738
nadpi <nadawaled87@gmail.com>
2023-11-17 15:03:14 -0800

    task 2

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -43,4 +43,4 @@ class Rectangle:
         if width or height is equal to 0, perimeter is equal to 0"""
         if self.__width == 0 or self.__height == 0:
             return 0
-        return (2 * (self.__width * self.__height))
+        return (2 * (self.__width + self.__height))
