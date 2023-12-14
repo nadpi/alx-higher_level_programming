c82c5bb018aa7f0c1a54220e624f7d59c5073c96
nadpi <nadawaled87@gmail.com>
2023-12-13 16:20:20 -0800

    [8. Cities of California]

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -3,4 +3,4 @@ SELECT hbtn_0d_usa.cities.id, hbtn_0d_usa.cities.name
 FROM hbtn_0d_usa.cities, hbtn_0d_usa.states
 WHERE hbtn_0d_usa.cities.state_id = hbtn_0d_usa.states.id
 AND hbtn_0d_usa.states.name = 'California'
-ORDER BY hbtn_0d_usa.cities.id
+ORDER BY hbtn_0d_usa.cities.id ASC
