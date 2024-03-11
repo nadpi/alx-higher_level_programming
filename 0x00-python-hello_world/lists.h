45ebd187c1fba21d9a81e1292477b56041418d6e
nada <nadadwaled87@gmail.com>
2024-03-11 13:59:21 -0700

    [10. Linked list cycle]

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -21,5 +21,4 @@ size_t print_listint(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
-int check_if(listint_t *head, listint_t *current);
 #endif /* LISTS_H */
