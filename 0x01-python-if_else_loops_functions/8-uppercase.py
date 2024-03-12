0fa094d75cd9a0cd038176ad119640ed56ea3640
nada <nadadwaled87@gmail.com>
2024-03-12 16:33:31 -0700

    [8. To uppercase]

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,7 @@
 def uppercase(str):
     for i in range(len(str)):
         if ord(str[i]) > 96 and ord(str[i]):
-            print(chr(ord(str[i]) - 32), end="")
+            print("{}".format(chr(ord(str[i]) - 32)), end="")
         else:
-            print(str[i], end="")
+            print("{}".format(str[i]), end="")
     print("")
