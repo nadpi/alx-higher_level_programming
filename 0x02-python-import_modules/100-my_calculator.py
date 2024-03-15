4070e1a6cdfab2088372d440dd13360931982437
nada <nadadwaled87@gmail.com>
2024-03-14 17:57:45 -0700

    [6. Build my own calculator!]

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     elif argv[2] == '*':
         print("{} {} {} = {}".format(a, argv[2], b, mul(a, b)))
     elif argv[2] == '/':
-        print("{} {} {} = {}".format(a, argv[2], b, add(a, b)))
+        print("{} {} {} = {}".format(a, argv[2], b, div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
