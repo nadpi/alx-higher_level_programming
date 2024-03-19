7f4c7c97c99cbda942d9647fa466e3b4335c095f
nada <nadadwaled87@gmail.com>
2024-03-19 16:25:54 -0700

    [6. Lists of lists = Matrix]

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     if matrix:
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
-                if j < len(matrix) - 1:
+                if j < len(matrix[0]) - 1:
                     print('{:d}'.format(matrix[i][j]), end=" ")
                 else:
                     print('{:d}'.format(matrix[i][j]), end="")
