6f7c94267f9714c0fd444834b86483573bf973dd
nada <nadadwaled87@gmail.com>
2024-03-19 16:41:26 -0700

    [7. Tuples addition]

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    tuple_c = ()
+    if len(tuple_a) == 0:
+        return tuple_b
+    elif len(tuple_b) == 0:
+        return tuple_a
+    elif len(tuple_a) < 2:
+        tuple_a += (0,)
+    elif len(tuple_b) < 2:
+        tuple_b += (0,)
+    for i in range(2):
+        tuple_c += (tuple_a[i] + tuple_b[i],)
+    return tuple_c
