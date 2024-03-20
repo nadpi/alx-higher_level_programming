e18bc2dd85ec3ed8591668de9d0f5cb6949cf942
nada <nadadwaled87@gmail.com>
2024-03-20 16:26:44 -0700

    [3. Present in both]

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -3,6 +3,7 @@ def common_elements(set_1, set_2):
     set_3 = set()
     for i in range(len(set_1)):
         for j in range(len(set_2)):
-            if list(set_1)[i] == list(set_2)[j] and list(set_2)[j] not in set_3:
-                set_3.add(list(set_2)[j])
+            if list(set_1)[i] == list(set_2)[j]:
+                if list(set_2)[j] not in set_3:
+                    set_3.add(list(set_2)[j])
     return set_3
