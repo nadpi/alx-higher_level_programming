4a0bf937e0c672a16f3f65429600cb35752b7483
nada <nadadwaled87@gmail.com>
2024-03-20 16:25:49 -0700

    [3. Present in both]

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    set_3 = set()
+    for i in range(len(set_1)):
+        for j in range(len(set_2)):
+            if list(set_1)[i] == list(set_2)[j] and list(set_2)[j] not in set_3:
+                set_3.add(list(set_2)[j])
+    return set_3
