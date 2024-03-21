a3bedf734b23be5caf38ec033838d1b37e90ce6d
nada <nadadwaled87@gmail.com>
2024-03-20 17:20:27 -0700

    [7. Update dictionary]

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if key in a_dictionary.keys():
-        a_dictionary.update({key: value})
-    else:
-        a_dictionary[key] = value
+    a_dictionary.update({key: value})
     return a_dictionary
