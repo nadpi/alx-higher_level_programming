573e3ecaa34f08d62160f5a5e23bb5a15d54c6e2
nada <nadadwaled87@gmail.com>
2024-03-20 17:59:03 -0700

    [10. Best score]

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
     maxnum = 0
-    whomaxnum = ""
+    whomaxnum = 0
     for i in a_dictionary:
         if a_dictionary[i] > maxnum:
             maxnum = a_dictionary[i]
