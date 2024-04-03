2cbcd7190045dcdabc7bd39a39b9013c5253a590
nada <nadadwaled87@gmail.com>
2024-04-02 18:52:52 -0700

    [2. Size validation]

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+''' Class Square'''
+
+
+class Square:
+    ''' class square has
+    Args:
+        size(int): size
+    '''
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
