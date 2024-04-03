73e8df64b10043d2a9152451e1dd462663a47846
nada <nadadwaled87@gmail.com>
2024-04-02 17:20:52 -0700

    [0. Safe list printing]

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        for i in range(x):
+            try:
+                print("{}".format(my_list[i]), end="")
+            except IndexError:
+                print("")
+                return i
+        print("")
+        return i + 1
+    except TypeError:
+        print("error")
+        return 0
