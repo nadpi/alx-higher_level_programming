170409e5dd0073a69d556e94ed1f0e76abeb550f
nada <nadadwaled87@gmail.com>
2024-04-02 18:27:49 -0700

    [1. Squar with size]

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,8 +3,9 @@
 
 
 class Square:
-    ''' attributes:
-    size(int): size
+    ''' A square class that has
+    Attributes:
+        size(int): size
     '''
     def __init__(self, size = 0):
         self.__size = size
