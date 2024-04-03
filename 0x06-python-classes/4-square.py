02d4807e402c00d5996e90fad99c8d679dca4692
nada <nadadwaled87@gmail.com>
2024-04-02 19:09:26 -0700

    [4. Access and update private attribute]

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -15,8 +15,8 @@ class Square:
         ''' 
         size must be an integer, otherwise raise a TypeError
         exception with the message size must be an integer
-        if size is less than 0, raise a ValueError exception
-        with the message size must be >= 0'''
+        if size is less than 0, raise a ValueError
+        exception with the message size must be >= 0'''
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
@@ -25,7 +25,7 @@ class Square:
 
     @property
     def size(self):
-        ''' retrieve size'''
+        ''' return size '''
         return self.__size
 
     def area(self):
