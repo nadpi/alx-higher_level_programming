461500857364616880103da1d0f6c45ecf84deeb
nada <nadadwaled87@gmail.com>
2024-04-08 16:47:58 -0700

    [6. Coordinates of a square]

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -61,7 +61,7 @@ class Square:
                 for k in range(self.__position[1]):
                     print()
             for i in range(self.__size):
-                if self.__position:
+                if self.__position[0] > 0:
                     for _ in range(self.__position[0]):
                         print(" ", end="")
                 for j in range(self.__size):
