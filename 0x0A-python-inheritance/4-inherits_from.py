1f97e1f955d90696eca3360e431949217c411ed6
nada <nadadwaled87@gmail.com>
2024-04-15 17:51:57 -0700

    [4. Only sub class of]

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''4. Only sub class of'''
+
+
+def inherits_from(obj, a_class):
+    '''a function that returns True if the object is an instance of a class
+    that inherited (directly or indirectly) from the specified class ;
+    otherwise False.'''
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
