a4103ec6605283d8c94a4e5847da2ec04e16cd01
nada <nadadwaled87@gmail.com>
2024-04-15 19:03:10 -0700

    [8. Rectangle]

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -8,7 +8,5 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 class Rectangle(BaseGeometry):
     ''' a class Rectangle that inherits from BaseGeometry '''
     def __init__(self, width, height):
-        if self.integer_validator("width", width):
-            self.__width = width
-        if self.integer_validator("height", height):
-            self.__height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+''' 9. Full rectangle '''
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    ''' a class Rectangle that inherits from BaseGeometry '''
+    def __init__(self, width, height):
+        super().__init__()
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        ''' area method '''
+        return self.__width * self.__height
+
+    def print(self):
+        ''' prints '''
+        print("[Rectangle] {}/{}".format(self.__width, self.__height))
