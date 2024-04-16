49f0dddf88a2de83fbe09090080d2b323be1b800
nada <nadadwaled87@gmail.com>
2024-04-15 18:41:45 -0700

    [8. Rectangle]

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -3,6 +3,8 @@
 
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
 class Rectangle(BaseGeometry):
     ''' a class Rectangle that inherits from BaseGeometry '''
     def __init__(self, width, height):
