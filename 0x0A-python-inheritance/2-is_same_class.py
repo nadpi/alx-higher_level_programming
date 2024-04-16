7110edce446af50b90fa1b17f2cd44cfa66daffd
nada <nadadwaled87@gmail.com>
2024-04-15 17:40:50 -0700

    [2. Exact same object]

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""2. Exact same object"""
+'''2. Exact same object'''
 
 
 def is_same_class(obj, a_class):
-    """a function that returns True if the object is exactly
-an instance of the specified class otherwise False."""
+    '''a function that returns True if the object is exactly
+    an instance of the specified class otherwise False'''
     if type(obj) == a_class:
         return True
     else:
