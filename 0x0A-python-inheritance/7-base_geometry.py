3929dd07999f87c909cde3fe6eae9924cf8e3ac0
nada <nadadwaled87@gmail.com>
2024-04-15 18:14:06 -0700

    [7. Integer validator]

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+''' 7. Integer validator '''
+
+
+class BaseGeometry:
+    '''class BaseGeometry'''
+    def area(self):
+        '''raises an Exception with the message area() is not implemented'''
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        '''validates value'''
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value < 0 or value == 0:
+            raise ValueError("{} must be greater than 0".format(name))
