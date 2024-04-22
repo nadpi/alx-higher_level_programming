b2a21c244abd0a82c188ae8cacab842563de993e
nada <nadadwaled87@gmail.com>
2024-04-22 16:35:37 -0700

    [2. First Rectangle]

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,6 +4,7 @@ from models.base import Base
 
 
 class Rectangle(Base):
+    '''class rect'''
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
         self.__width = width
