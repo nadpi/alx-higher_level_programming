2ef0c43be7ceca47b3149fa2ca8958eef6c4c991
nada <nadadwaled87@gmail.com>
2024-04-22 16:13:03 -0700

    [1. Base class]

--- 0x0C-python-almost_a_circle/tests/1-test.py
+++ 0x0C-python-almost_a_circle/tests/1-test.py
@@ -2,15 +2,16 @@
 import unittest
 from models.base import Base
 
-
 class TestBaseClass(unittest.TestCase):
-	def oneBase(self):
-		self.b1 = Base()
-	def TwoBase(self):
-		self.b2 = Base()
-		self.b2 = Base(0)
-	def ThreeBase(self):
-		self.b3 = Base(12)
+    def testoneBase(self):
+        self.b1 = Base()
+
+    def testTwoBase(self):
+        self.b2 = Base()
+        self.b2 = Base(0)
+
+    def testThreeBase(self):
+        self.b3 = Base(12)
 
 if __name__ == '__main__':
     unittest.main()
