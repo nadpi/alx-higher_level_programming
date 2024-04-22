e378a235991e14541a90b79d3b789629c9d22bbe
nada <nadadwaled87@gmail.com>
2024-04-22 16:29:02 -0700

    [2. First Rectangle]

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''Task 3'''
+from models.base import Base
+
+
+class Rectangle(Base):
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
