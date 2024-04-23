1a9de8202df1edaf588230b40072f9f960b6a3d1
nada <nadadwaled87@gmail.com>
2024-04-22 18:37:23 -0700

    [11. Square size]

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -14,3 +14,17 @@ class Square(Rectangle):
         strtoret += str(self.x)+"/"+str(self.y)+" - "
         strtoret += str(self.width)
         return strtoret
+
+    @property
+    def size(self):
+        '''ret size'''
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        '''set size'''
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.width = value
