84c34e2e283dec0785f3a06a4eef78b7adeb473b
nada <nadadwaled87@gmail.com>
2024-04-22 17:50:06 -0700

    [7. Display #1]

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -78,8 +78,8 @@ class Rectangle(Base):
             for k in range(self.__y):
                 print()
         for i in range(self.__height):
-            for l in range(self.__x):
-                        print(" ", end="")
+            for ll in range(self.__x):
+                print(" ", end="")
             for j in range(self.__width):
                 print("#", end="")
             print()
