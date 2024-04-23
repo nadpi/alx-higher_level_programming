238931c2c86a22044a7ea149a4447fd39b7bf027
nada <nadadwaled87@gmail.com>
2024-04-22 18:33:02 -0700

    [10. And now, the Square!]

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -6,7 +6,7 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     '''class square'''
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(size, size, x, y)
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
         '''prints'''
