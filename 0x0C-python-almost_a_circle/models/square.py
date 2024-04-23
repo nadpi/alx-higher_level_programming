bb257395ce35610d901d36d0e72d2d6d92e53aa9
nada <nadadwaled87@gmail.com>
2024-04-22 18:31:48 -0700

    [10. And now, the Square!]

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+''' Task 10'''
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    '''class square'''
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y)
+
+    def __str__(self):
+        '''prints'''
+        strtoret = "[Square]"+" ("+str(self.id)+") "
+        strtoret += str(self.x)+"/"+str(self.y)+" - "
+        strtoret += str(self.width)
+        return strtoret
