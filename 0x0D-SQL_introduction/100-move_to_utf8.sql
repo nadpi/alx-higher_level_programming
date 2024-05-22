7778bf88441c4bc589a10b2aa13fb621da48a0ec
nada <nadadwaled87@gmail.com>
2024-05-21 19:13:21 -0700

    [17. Go to UTF8]

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -8,9 +8,8 @@ ALTER TABLE
     CONVERT TO CHARACTER SET utf8mb4
     COLLATE utf8mb4_unicode_ci;
 
-ALTER TABLE
-    hbtn_0c_0.first_table
-    MODIFY name
-    VARCHAR(256)
-    CHARACTER SET utf8mb4
-    COLLATE utf8mb4_unicode_ci;
+ALTER TABLE hbtn_0c_0.first_table
+    MODIFY COLUMN id int(11) DEFAULT NULL;
+
+ALTER TABLE hbtn_0c_0.first_table
+    MODIFY COLUMN name varchar(256) COLLATE utf8mb4_unicode_ci DEFAULT NULL;
