34f25eff5344da4c22d6acdc9bc978521ff54481
nada <nadadwaled87@gmail.com>
2024-06-18 17:12:34 -0700

    [10. Factorial]

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
-if (argv.length === 2) {
-  console.log(1);
-} else {
-  let num = Number(argv[2]);
+let num = Number(argv[2]);
+function fact (num) {
   let factorial = 1;
   for (num; num > 0; num--) {
     factorial *= num;
-  }
-  console.log(factorial);
+ }
+  return factorial;
 }
+console.log(fact(num));
