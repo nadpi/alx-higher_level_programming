186429027412bdebe8328107b2caded769ba09d8
nada <nadadwaled87@gmail.com>
2024-06-18 17:31:36 -0700

    [11. Second biggest!]

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const { argv } = require('node:process');
+if (argv.length < 4) {
+  console.log(0);
+} else {
+  let big = Number(argv[2]);
+  for (let i = 3; i < argv.length; i++) {
+    if (argv[i] > big) {
+      big = argv[i];
+    }
+  }
+  let secbig = 0;
+  for (let i = 2; i < argv.length; i++) {
+    if (argv[i] > secbig && argv[i] !== big) {
+      secbig = argv[i];
+    }
+  }
+  console.log(secbig);
+}
